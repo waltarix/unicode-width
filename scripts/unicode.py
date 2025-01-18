d61bd65da59cf0969c30b2899995238391b02565
waltari <gitting.crew@gmail.com>
2025-01-18 17:19:05 +0900

    custom

--- scripts/unicode.py
+++ scripts/unicode.py
@@ -448,11 +448,11 @@ def load_east_asian_widths() -> list[EastAsianWidth]:
             width_map.append(EastAsianWidth.NARROW)
 
     # Characters with ambiguous line breaking are ambiguous
-    load_property(
-        "LineBreak.txt",
-        "AI",
-        lambda cp: (operator.setitem(width_map, cp, EastAsianWidth.AMBIGUOUS)),
-    )
+    # load_property(
+    #     "LineBreak.txt",
+    #     "AI",
+    #     lambda cp: (operator.setitem(width_map, cp, EastAsianWidth.AMBIGUOUS)),
+    # )
 
     # Ambiguous `Letter`s and `Modifier_Symbol`s are narrow
     load_property(
@@ -983,7 +983,7 @@ def make_tables(
     table, and so forth. `entries` is an iterator over the `(Codepoint, EffectiveWidth)` pairs
     to include in the top-level table."""
 
-    entries = enumerate([w.table_width() for w in width_map])
+    entries = enumerate([w.table_width() for w in cjk_width_map])
     cjk_entries = enumerate([w.table_width() for w in cjk_width_map])
 
     root_table = Table(
@@ -2040,115 +2040,6 @@ static EMOJI_MODIFIER_LEAF_{leaf_idx}: [(u8, u8); {len(leaf)}] = [
             for lo, hi in leaf:
                 module.write(f"    (0x{lo:02X}, 0x{hi:02X}),\n")
             module.write(f"];\n")
-
-        test_width_variants = []
-        test_width_variants_cjk = []
-        for variant in WidthState:
-            if variant.is_carried():
-                if not variant.is_cjk_only():
-                    test_width_variants.append(variant)
-                if not variant.is_non_cjk_only():
-                    test_width_variants_cjk.append(variant)
-
-        module.write(
-            f"""
-#[cfg(test)]
-mod tests {{
-    use super::*;
-
-    fn str_width_test(s: &str, init: WidthInfo) -> isize {{
-        s.chars()
-            .rfold((0, init), |(sum, next_info), c| -> (isize, WidthInfo) {{
-                let (add, info) = width_in_str(c, next_info);
-                (sum.checked_add(isize::from(add)).unwrap(), info)
-            }})
-            .0
-    }}
-
-    #[cfg(feature = "cjk")]
-    fn str_width_test_cjk(s: &str, init: WidthInfo) -> isize {{
-        s.chars()
-            .rfold((0, init), |(sum, next_info), c| -> (isize, WidthInfo) {{
-                let (add, info) = width_in_str_cjk(c, next_info);
-                (sum.checked_add(isize::from(add)).unwrap(), info)
-            }})
-            .0
-    }}
-
-    #[test]
-    fn test_normalization() {{
-        for &(orig, nfc, nfd, nfkc, nfkd) in &NORMALIZATION_TEST {{
-            for init in NORMALIZATION_TEST_WIDTHS {{
-                assert_eq!(
-                    str_width_test(orig, init),
-                    str_width_test(nfc, init),
-                    "width of X = {{orig:?}} differs from toNFC(X) = {{nfc:?}} with mode {{init:X?}}",
-                );
-                assert_eq!(
-                    str_width_test(orig, init),
-                    str_width_test(nfd, init),
-                    "width of X = {{orig:?}} differs from toNFD(X) = {{nfd:?}} with mode {{init:X?}}",
-                );
-                assert_eq!(
-                    str_width_test(nfkc, init),
-                    str_width_test(nfkd, init),
-                    "width of toNFKC(X) = {{nfkc:?}} differs from toNFKD(X) = {{nfkd:?}} with mode {{init:X?}}",
-                );
-            }}
-
-            #[cfg(feature = "cjk")]
-            for init in NORMALIZATION_TEST_WIDTHS_CJK {{
-                assert_eq!(
-                    str_width_test_cjk(orig, init),
-                    str_width_test_cjk(nfc, init),
-                    "CJK width of X = {{orig:?}} differs from toNFC(X) = {{nfc:?}} with mode {{init:X?}}",
-                );
-                assert_eq!(
-                    str_width_test_cjk(orig, init),
-                    str_width_test_cjk(nfd, init),
-                    "CJK width of X = {{orig:?}} differs from toNFD(X) = {{nfd:?}} with mode {{init:X?}}",
-                );
-                assert_eq!(
-                    str_width_test_cjk(nfkc, init),
-                    str_width_test_cjk(nfkd, init),
-                    "CJK width of toNFKC(X) = {{nfkc:?}} differs from toNFKD(X) = {{nfkd:?}} with mode {{init:?}}",
-                );
-            }}
-        }}
-    }}
-
-    static NORMALIZATION_TEST_WIDTHS: [WidthInfo; {len(test_width_variants) + 1}] = [
-        WidthInfo::DEFAULT,\n"""
-        )
-
-        for variant in WidthState:
-            if variant.is_carried() and not variant.is_cjk_only():
-                module.write(f"        WidthInfo::{variant.name},\n")
-
-        module.write(
-            f"""    ];
-
-    #[cfg(feature = "cjk")]
-    static NORMALIZATION_TEST_WIDTHS_CJK: [WidthInfo; {len(test_width_variants_cjk) + 1}] = [
-        WidthInfo::DEFAULT,\n"""
-        )
-
-        for variant in WidthState:
-            if variant.is_carried() and not variant.is_non_cjk_only():
-                module.write(f"        WidthInfo::{variant.name},\n")
-
-        module.write(
-            f"""    ];
-
-    #[rustfmt::skip]
-    static NORMALIZATION_TEST: [(&str, &str, &str, &str, &str); {len(normalization_tests)}] = [\n"""
-        )
-        for orig, nfc, nfd, nfkc, nfkd in normalization_tests:
-            module.write(
-                f'        (r#"{orig}"#, r#"{nfc}"#, r#"{nfd}"#, r#"{nfkc}"#, r#"{nfkd}"#),\n'
-            )
-
-        module.write("    ];\n}\n")
 
 
 def main(module_path: str):
